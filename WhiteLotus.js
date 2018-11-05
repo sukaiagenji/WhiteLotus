182247f7e67a82b2a53b2f29377b83ce0f12d0c8
sukaiagenji <sukaiagenji@gmail.com>
2018-11-05 13:17:16 -0500

    Primary creation of install script

--- WhiteLotus.js
+++ WhiteLotus.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var install = require('./install');
-
-if (!fs.existsSync('./config.json')) {
-	console.log('Beginning White Lotus installation...')
-	install.startInstall();
+try {
+	var express = require('express');
+	var app = express();
+	var fs = require('fs');
+	var install = require('./install');
+}
+catch(error) {
+	console.log(error);
 }
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+if (!fs.existsSync('./config.json')) {
+	console.log('Beginning White Lotus installation...');
+	install.startInstallation();
+}
+
+if (fs.existsSync('./config.json')) {
+	app.get('/', function (req, res) {
+		res.send('Hello World!');
+	});
+	app.listen(3000, function () {
+		console.log('Example app listening on port 3000!');
+	});
+}
+
--- install.js
+++ install.js
@@ -3,21 +3,21 @@ exports.startInstallation = function () {
 
 	console.log('***************************\n**     Welcome to the    **\n**      White\u2740Lotus      **\n**       Installer       **\n***************************\n');
 
-	console.log('\nThis installer will add the Alexa AVS Sample App to your Raspberry Pi.\nA Raspberry Pi 3 is highly recommended, although a Raspberry Pi 2 Model B+ is supported.'
-	console.log('\nDesktop is not recommended, since everything, including card rendering, is created in console.')
-	console.log('\nBecause of the amount of processing necessary to run the Alexa AVS Sample App,\nIt is recommended that you have no other applications running on this Raspberry Pi.'
+	console.log('\nThis installer will add the Alexa AVS Sample App to your Raspberry Pi.\nA Raspberry Pi 3 is highly recommended, although a\nRaspberry Pi 2 Model B+ is supported.');
+	console.log('\nDesktop is not recommended, since everything, including card rendering,\nis created in console.');
+	console.log('\nBecause of the amount of processing necessary to run the Alexa AVS Sample App,\nit is recommended that you have no other applications running on this\nRaspberry Pi.');
 
 	var rl = readline.createInterface({
-		input: process.stdin
-	}
+		input: process.stdin,
+		output: process.stdout
+	});
 
-	rl.question('\nAre you ready to proceed with installing the Alexa AVS Sample App and all dependencies? (yes/No)\n' (answeryesno) => {
-		if(answeryesno.toLowerCase == 'yes') {
+	rl.question('\nAre you ready to proceed with installing the\nAlexa AVS Sample App and all dependencies? (yes/No)\n', (answeryesno) => {
+		if(answeryesno.toLowerCase() == 'yes') {
 			console.log('\n\nProceeding with installation...');
 			doInstallation();
 		} else {
 			console.log('\n\nInstallation aborted!!!');
-			break;
 		}
 
 		rl.close();
